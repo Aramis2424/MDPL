4a41290228a204e102f6df4f5c01076da11ec0ca
Aramis2424 <srd2424@yandex.ru>
2022-04-03 12:41:40 +0300

    init lab_05

---
+++ lab_05/print.asm
@@ -0,0 +1,21 @@
+public print
+extrn number : byte
+
+
+
+CODE_SEG_2 SEGMENT PARA 'CODE'
+	ASSUME CS:CODE_SEG_2
+	print:
+		sub number, 5 ;уменьшение числа на 5
+		
+		mov AH, 02h
+		mov DL, 20h ;пробел
+		int 21h
+		mov DL, number
+		int 21h
+		
+		mov AH, 4Ch
+		int 21h
+		
+CODE_SEG_2 ENDS
+END
