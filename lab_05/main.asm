ac8777e047aed9011c66c39d2a8aed46fcdb018e
Aramis2424 <srd2424@yandex.ru>
2022-04-07 12:21:09 +0300

    made input and output matrix

--- lab_05/main.asm
+++ lab_05/main.asm
@@ -24,11 +24,10 @@ DataSeg SEGMENT PARA 'DATA'
 	MIN_I DW 1 DUP (0)
 DataSeg ENDS	
 
-
 CodeSeg SEGMENT PARA 'CODE'	
     ASSUME CS:CodeSeg, DS:TextSeg, SS:StackSeg
 	
-input_num:
+input_sym:
 	MOV AH, 01h
 	INT 21h
 	RET
@@ -47,7 +46,7 @@ print_space:
 	INT 21h
 	RET
 
-print_num:
+print_sym:
 	MOV AH, 02h
 	INT 21h
 	RET
@@ -58,27 +57,27 @@ print_text:
 	RET
 
 main:
-	MOV AX, DataSeg
+	MOV AX, DataSeg ;записывание data
     MOV ES, AX
 	
-	MOV AX, TextSeg
+	MOV AX, TextSeg ;записывание text
 	MOV DS, AX
 
-	MOV DX, OFFSET ES:MSG_ROW
+	MOV DX, OFFSET ES:MSG_ROW ;сообщение на ввод строки
 	CALL print_text
-	CALL input_num
+	CALL input_sym ;ввод числа строк
 	
-	MOV ES:ROW, AL
-	SUB ES:ROW, 30h
-	MOV AL, ES:ROW
-	;CALL print_new_line ;;
+	MOV ES:ROW, AL ;перенос из al в row количества строк
+	SUB ES:ROW, 30h ;делаем из '<число>' <число>
+	MOV AL, ES:ROW ;переносим для проверки кол-во строк в al
+	;CALL print_new_line ;
 	CMP AL, 0
-	JE error_exit ;;
-	CALL print_new_line ;;
+	JE error_exit ;
+	CALL print_new_line ;новая строка
 	
-	MOV DX, OFFSET ES:MSG_COL
+	MOV DX, OFFSET ES:MSG_COL ;то же, что и для row
 	CALL print_text
-	CALL input_num
+	CALL input_sym
 	MOV ES:COL, AL
 	SUB ES:COL, 30h
 	MOV AL, ES:COL
@@ -87,119 +86,121 @@ main:
 	JE error_exit
 	
 	
-	MOV DX, OFFSET ES:MSG_MTR
+	MOV DX, OFFSET ES:MSG_MTR ;приглашение на ввод матрицы
 	CALL print_text
 	CALL print_new_line
 	
-	input_mtr:
-		XOR BX, BX ;обнуление
-		XOR CX, CX
-		MOV CL, ES:ROW
+	input_mtr: ;ввод матрицы
+		XOR BX, BX ;обнуление (скорее всего делать не обязательно)
+		XOR CX, CX 
+		MOV CL, ES:ROW ;переносим кол-во строк в cl
 		XOR SI, SI
-		input_row:
-			PUSH CX
-			MOV CL, ES:COL
-			XOR BX, BX
-			input_col:
-				CALL input_num
-				SUB AL, 30h
-				MOV ES:MTR[BX + SI], AL
+		input_row: ;начало цикла
+			PUSH CX ;Сохраняем кол-во строк, которые еще нужно прочитать, отправляя его в стек
+			MOV CL, ES:COL ;переносим кол-во столбцов в cl
+			XOR BX, BX ;на новой строке начнем заполнять с начала
+			input_col: ;начало цикла
+				CALL input_sym ;ввод элемента матрицы
+				SUB AL, 30h ;делаем из него число
+				MOV ES:MTR[BX + SI], AL ;запись элемента матрицы в  ячейку матрицы (bx = 0, 1, ..., 8 - текущий столбец ## si = 0, 9, 18, ..., 72 - текущая строка)
 				
-				INC BX
+				INC BX ;следующая ячейка в этой же строке
 				CALL print_space
-				LOOP input_col			
+				LOOP input_col ;конец цикла (здесь на один уменьшается кол-во столбцов, которые еще нужно прочитать)
+				
 			CALL print_new_line
-			POP CX
-			ADD SI, 9
-			LOOP input_row
-		
-		
-	find_max_min: 
-		MOV SI, 0
-		
-		XOR AX, AX
-		MOV AL, ES:COL
-		XOR DI, DI
-		MOV DI, AX
-		
-		XOR CX, CX
-		MOV CL, ES:ROW
-		
-		XOR AX, AX
-		XOR DX, DX
-		MOV AL, ES:MTR[SI]
-		MOV DL, ES:MTR[SI]
-		
-		find_row_max_min:
-			PUSH CX
-			MOV CL, ES:COL
-			MOV BX, 0
-			find_col_max_min:
-				CMP AL, ES:MTR[SI][BX]
-				JLE next_max
-				CMP DL, ES:MTR[SI][BX]
-				JGE next_min
-				JMP next
-			next_max:
-				MOV AL, ES:MTR[SI][BX]
-				MOV ES:MAX_I, SI
-				JMP next 
-			next_min:
-				MOV DL, ES:MTR[SI][BX]
-				MOV ES:MIN_I, SI	
-
-			next:
-				INC BX
-				LOOP find_col_max_min
-			POP CX
-			ADD SI, DI
-			LOOP find_row_max_min
+			POP CX ;получаем из стека сохраненное значение кол-ва строк
+			ADD SI, 9 ;переходим на новую строку
+			LOOP input_row ;конец цикла (здесь на один уменьшается кол-во строк, которые еще нужно прочитать)
 			
-	swap_lines:
-		XOR CX, CX
-		MOV CL, ES:COL
-		MOV SI, ES:MAX_I
-		MOV BX, ES:MIN_I
-		swap:
-			XCHG AL, ES:MTR[SI]
-			XCHG AL, ES:MTR[BX]
-			XCHG AL, ES:MTR[SI]
-			INC SI
-			INC BX
-			LOOP swap
+	; find_max_min: 
+		; MOV SI, 0
+		
+		; XOR AX, AX
+		; MOV AL, ES:COL
+		; XOR DI, DI
+		; MOV DI, AX
+		
+		; XOR CX, CX
+		; MOV CL, ES:ROW
+		
+		; XOR AX, AX
+		; XOR DX, DX
+		; MOV AL, ES:MTR[SI]
+		; MOV DL, ES:MTR[SI]
+		
+		; find_row_max_min:
+			; PUSH CX
+			; MOV CL, ES:COL
+			; MOV BX, 0
+			; find_col_max_min:
+				; CMP AL, ES:MTR[SI][BX]
+				; JLE next_max
+				; CMP DL, ES:MTR[SI][BX]
+				; JGE next_min
+				; JMP next
+			; next_max:
+				; MOV AL, ES:MTR[SI][BX]
+				; MOV ES:MAX_I, SI
+				; JMP next 
+			; next_min:
+				; MOV DL, ES:MTR[SI][BX]
+				; MOV ES:MIN_I, SI	
+
+			; next:
+				; INC BX
+				; LOOP find_col_max_min
+			; POP CX
+			; ADD SI, DI
+			; LOOP find_row_max_min
+			
+	; swap_lines:
+		; XOR CX, CX
+		; MOV CL, ES:COL
+		; MOV SI, ES:MAX_I
+		; MOV BX, ES:MIN_I
+		; swap:
+			; XCHG AL, ES:MTR[SI]
+			; XCHG AL, ES:MTR[BX]
+			; XCHG AL, ES:MTR[SI]
+			; INC SI
+			; INC BX
+			; LOOP swap
 	
-	print_mtr:
-		MOV DX, OFFSET ES:MSG_RES
+	print_mtr: ;вывод матрицы
+		MOV DX, OFFSET ES:MSG_RES ;print('result')
 		CALL print_text
 		CALL print_new_line
-		XOR BX, BX
+		
+		;XOR BX, BX
 		XOR CX, CX
-		MOV CL, ES:ROW
+		MOV CL, ES:ROW ;для вывода -- кол-во строк
+		XOR SI, SI
 		print_row:
-			XOR BX, BX
-			PUSH CX 
+			PUSH CX ;тот же финт, что и при вводе
 			MOV CL, ES:COL
-			print_col:
+			XOR BX, BX
+			print_col: ;для вывода -- кол-во столбцов
 				MOV DL, ES:MTR[BX + SI]
-				ADD DL, 30h
-				CALL print_num
+				ADD DL, 30h ;делаем из числа строку
+				CALL print_sym
 				INC BX
 				CALL print_space
 				LOOP print_col
 			CALL print_new_line
+			
 			POP CX
 			ADD SI, 9
 			LOOP print_row
 			
-
-	MOV AH, 4Ch						
+	MOV AH, 4Ch	;завершаем				
     INT 21h
 	
 	error_exit:
 		CALL print_new_line
-		MOV DX, OFFSET ES:MSG_ERR
+		MOV DX, OFFSET ES:MSG_ERR ;печатаем сообщение об ошибке
 		CALL print_text
-		MOV AH, 4Ch		
+		MOV AH, 4Ch	;завершаем
 
 		INT 21h
 	
