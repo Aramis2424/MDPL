cec878aab768cc3699ba3c79aec5ccb9a2cd1922
Aramis2424 <srd2424@yandex.ru>
2022-05-05 12:16:32 +0300

    add comments

--- lab_09/sample/my_sin.c
+++ lab_09/sample/my_sin.c
@@ -6,15 +6,16 @@ void sin_asm()
 {
     double res;
 
-    printf("Simple test sin(3.14): %.15g\n", sin(3.14));
-    printf("Simple test sin(3.141596): %.15g\n", sin(3.141596));
+    printf("Standart func: sin(3.14): %.15g\n", sin(3.14));
+    printf("Standart func: sin(3.141596): %.15g\n", sin(3.141596));
 
 #ifdef FPUX87
     __asm__ 
     (
-        "fldpi\n\t"
-        "fsin\n\t"
-        "fstp %0\n\t" 
+		// ST соответствует регистру - текущей вершине стека, ST(1)..ST(7) - прочие регистры
+        "fldpi\n\t" // число пи (константа FPU), устанавливает число пи в вершину стека ( St(0) )
+        "fsin\n\t" // Команда FSIN замещает содержимое ST значением sin(ST)
+        "fstp %0\n\t"  // считать число с вершины стека в приёмик
         ::"m"(res));
     printf("Sin test fpu: %.15g\n", res);
 #endif
