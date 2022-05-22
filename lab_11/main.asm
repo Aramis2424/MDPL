24f8d9288075090ad223800b14049e157c04d27b
Aramis2424 <srd2424@yandex.ru>
2022-05-22 20:07:22 +0300

    add comments

--- lab_11/main.asm
+++ lab_11/main.asm
@@ -1,54 +1,64 @@
-format PE GUI 4.0
-entry start
+format PE GUI 4.0 ; говорим компилятору FASM какой файл делать
+entry start ; говорим windows-у, где начало программы
 
-include 'win32a.inc'
+include 'win32a.inc' ; подключаем библиотеку FASM-а 
 
 ID_FIRSTDIG          = 101
 ID_SECONDDIG         = 102
 ID_RES               = 103 
 
-section '.text' code readable executable
-
-  start:
+section '.text' code readable executable ; секция данных+кода
+; invoke - макрос, макро команда, т.е. fasm вместо invoke
+; подставит нужный набор команд
+  start:  ;метка старта
         invoke  GetModuleHandle,0
+		; Переход в основную процедуры программы
         invoke  DialogBoxParam,eax,37,HWND_DESKTOP,DialogProc,0
+		; Как только вышли из процедуры, заканчиваем программу
 		invoke  ExitProcess,0
 
 proc DialogProc hwnddlg,msg,wparam,lparam
         push    ebx esi edi
-        cmp     [msg],WM_COMMAND
-        je      .wmcommand
+		; Анализируем полученную команду
+        cmp     [msg],WM_COMMAND 
+        je      .wmcommand ; Выполнение сложения
         cmp     [msg],WM_CLOSE
-        je      .wmclose
+        je      .wmclose ; Завершение процедуры
         xor     eax,eax
         jmp     .finish
   .wmcommand:
         cmp     [wparam],BN_CLICKED shl 16 + IDCANCEL
         je      .wmclose
-        cmp     [wparam],BN_CLICKED shl 16 + IDOK
+        cmp     [wparam],BN_CLICKED shl 16 + IDOK ; Нажата кнопка для суммирования
         jne     .processed
-        invoke  GetDlgItemText,[hwnddlg],ID_FIRSTDIG,firstDigit,4h
+		
+		; Считывание первого числа из поля ввода в переменную
+        invoke  GetDlgItemText,[hwnddlg],ID_FIRSTDIG,firstDigit,4h 
         invoke  GetDlgItemText,[hwnddlg],ID_SECONDDIG,secondDigit,4h
         mov     [flags],MB_OK
 		
-		mov 	edx, firstDigit
+		mov 	edx, firstDigit ; Считывание в dx первого числа
 		mov 	edx, [edx]
-		sub 	edx, '0'
-		mov 	eax, secondDigit
-		mov 	eax, [eax]
-		sub 	eax, '0'
-		add 	eax, edx
+		sub 	edx, '0' ; Превращение первого числа из строки в, собственно, число
 		
-		cmp 	al, 10
-		jae 	.twoDigOut
-		;вывод 1 цифры
-		add 	eax, '0'
+		mov 	eax, secondDigit ; Считывание в dx второго числа
+		mov 	eax, [eax]
+		sub 	eax, '0' ; Превращение второго числа из строки в, собственно, число
+		
+		add 	eax, edx ; Сложение двух цифр
+		
+		cmp 	al, 10 ; Проверка полученного числа: больше ли оно 10 или нет
+		
+		jae 	.twoDigOut ; Если полученное число меньше 10
+		;вывод одной цифры
+		add 	eax, '0' ; Делаем из цифры строку
 		mov 	[res], al
-		mov 	[res + 1], ' '
-		invoke SetDlgItemText,[hwnddlg],ID_RES,res
+		mov 	[res + 1], ' ' ; Формируем результат
+		invoke SetDlgItemText,[hwnddlg],ID_RES,res ; Собственно вывод результата
 		jmp 	.processed
-  .twoDigOut:
-		;вывод 2 цифр
+		
+  .twoDigOut: ; То же, но в том случае, если результат больше или равен 10
+		;вывод двух цифр
 		mov 	dl, 31h
 		mov 	[res], dl
 		sub 	eax, 10
@@ -56,6 +66,7 @@ proc DialogProc hwnddlg,msg,wparam,lparam
 		mov 	[res + 1], al 
 		invoke SetDlgItemText,[hwnddlg],ID_RES,res
 		jmp 	.processed
+		
   .wmclose:
         invoke  EndDialog,[hwnddlg],0
   .processed:
@@ -72,10 +83,10 @@ section '.bss' readable writeable
   secondDigit db 4h
   res db 4h
 
-section '.idata' import data readable writeable
+section '.idata' import data readable writeable ; секция импорта
 
-  library kernel,'KERNEL32.DLL',\
-          user,'USER32.DLL'
+  library kernel,'KERNEL32.DLL',\ ; library как invoke - макро команда
+          user,'USER32.DLL' ; загрузка библиотеки
 
   import kernel,\
          GetModuleHandle,'GetModuleHandleA',\
@@ -96,6 +107,7 @@ section '.rsrc' resource data readable
   resource dialogs,\
            37,LANG_ENGLISH+SUBLANG_DEFAULT,demonstration
 
+  ; Размещение главного окна и объектов на нем
   ; Формат определения размеров объектов: X1,Y1, X2,<Высота>
   dialog demonstration,'Sum of digits',70,70,190,175,WS_CAPTION+WS_POPUP+WS_SYSMENU+DS_MODALFRAME ; Окно программы
   
@@ -107,6 +119,6 @@ section '.rsrc' resource data readable
 	
 	dialogitem 'BUTTON','Sum digits!',IDOK,10,80,170,15,WS_VISIBLE+WS_TABSTOP+BS_DEFPUSHBUTTON ; Кнопка
 	
-	dialogitem 'STATIC','&Result:',-1,110,70,70,8,WS_VISIBLE
+	dialogitem 'STATIC','&Result:',-1,10,110,70,8,WS_VISIBLE
     dialogitem 'EDIT','',ID_RES,10,120,170,15,WS_VISIBLE+WS_BORDER+WS_TABSTOP
   enddialog
